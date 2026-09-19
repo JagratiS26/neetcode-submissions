21bc0a881fa0184136fb0f3171f57bfd04b29452
Jagrati <jagratiindushukla@gmail.com>
2026-09-19 20:20:00 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sortArray(int[] nums) 
+    {
+    PriorityQueue<Integer>pq=new PriorityQueue<>();
+    for(int i:nums)
+      pq.add(i);
+    for(int i=0;i<nums.length;i++)
+     nums[i]=pq.poll();
+    return nums;    
+    }
+}

83e432cc8abf726ec41dcfc01b5229b45492d067
Jagrati <jagratiindushukla@gmail.com>
2026-09-21 16:46:43 +0530

    Add: lemonade-change - submission-2

---
+++ Data Structures & Algorithms/lemonade-change/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) 
+    {
+        int sum=0 ,five=0,ten=0,twenty=0;
+    for(int i=0;i<bills.length;i++)
+    {
+       if(bills[i]==5)
+       {
+         five++;
+         continue;
+       }
+       else if(bills[i]==10)
+       {
+        ten++;
+        if(five>0)
+         five--;
+        else
+         return false;
+       }
+       else if(bills[i]==20)
+       {
+        if(five>0&&ten>0)
+         {
+            five--;
+            ten--;
+         }
+         else if(five>=3)
+          five-=3;
+         else
+          return false;
+       }
+    
+    }    
+    return true;
+    }
+}
